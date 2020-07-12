ed7ae485bf630cd45f04844831911add5b1d3dc5
NT <pntn79@gmail.com>
2020-07-12 19:55:19 +0700

    Release where Date range

--- sql/query_test.go
+++ sql/query_test.go
@@ -1,6 +1,7 @@
 package sql_test
 import (
 	"crypto/md5"
+	"github.com/guregu/null"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"github.com/vietta-net/agokit/pb"
 	"github.com/vietta-net/agokit/sql"
@@ -15,28 +16,15 @@ import (
 )
 
 type User struct {
-	gorm.Model
 	ID string `gorm:"primary_key;column:id;type:char;size:36;" json:"id"`
 	Username string `gorm:"column:username;type:varchar;size:30;" json:"name"`
 	Password string `gorm:"column:password;type:varchar;size:32;" json:"name"`
+	CreatedAt time.Time `gorm:"column:created_at;type:datetime;" json:"created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime;" json:"updated_at"`
+	DeletedAt null.Time `gorm:"column:deleted_at;type:datetime;" json:"deleted_at"`
 }
 
 func TestBuildWhereDateRange(t *testing.T) {
-	format := "2006-01-02"
-	from, _ := time.Parse(format, "2010-07-13")
-	to, _ := time.Parse(format, "2010-07-13")
-
-	date := &pb.DateRange{
-		From: &timestamppb.Timestamp{
-			Seconds: from.Unix(),
-			Nanos:   0,
-		},
-		To: &timestamppb.Timestamp{
-			Seconds: to.Unix(),
-			Nanos:   0,
-		},
-	}
-
 	dataSource := "dev:passw0rd@tcp(127.0.0.1:3306)/dev?parseTime=true&charset=utf8mb4"
 	db, err := gorm.Open("mysql", dataSource)
 	assert.Nil(t, err)
@@ -48,9 +36,32 @@ func TestBuildWhereDateRange(t *testing.T) {
 
 	db.FirstOrCreate(&user, user)
 
+
+	format := "2006-01-02"
+	from, _ := time.Parse(format, "2010-07-13")
+
+	to := user.CreatedAt.Add(time.Hour * 24)
+
+	date := &pb.DateRange{
+		From: &timestamppb.Timestamp{
+			Seconds: from.Unix(),
+			Nanos:   0,
+		},
+		To: &timestamppb.Timestamp{
+			Seconds: to.Unix(),
+			Nanos:   0,
+		},
+	}
 	resultOrm := db.Model(&user )
 	timezone  := "Asia/Ho_Chi_Minh"
+
+	resultOrm.Where("id = ?", user.ID)
 	resultOrm, err = sql.BuildWhereDateRange(resultOrm, date, timezone)
 	assert.Nil(t, err)
+
+	var results = []User{}
+	resultOrm.Find(&results)
+
+	assert.Equal(t, 1, len(results))
 
 }
